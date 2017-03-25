bd0a061c381d845181e9c2045fee812031e461a7
github-user001 <2782982278@qq.com>
2017-03-25 11:22:12 +0800

    爬虫运行方法

---
+++ lbldy/lbldy/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = '主方法'
+__author__ = 'zhangjun'
+__mtime__ = '2017/3/15'
+   
+"""
+from scrapy import cmdline
+
+cmdline.execute("scrapy crawl lblSpider".split())
